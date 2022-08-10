e749dafcbb5c4208948904da786e78ab425b4e7d
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-10 15:23:17 -0500

    v0.7.0 Se integra CRUD de entidad fc_factura

---
+++ templates/directivas/fc_factura_html.php
@@ -0,0 +1,18 @@
+<?php
+namespace html;
+
+
+use gamboamartin\errores\errores;
+use gamboamartin\organigrama\controllers\controlador_org_empresa;
+use gamboamartin\system\html_controler;
+
+use models\base\limpieza;
+use models\org_empresa;
+use PDO;
+use stdClass;
+
+
+class fc_factura_html extends html_controler {
+
+
+}
