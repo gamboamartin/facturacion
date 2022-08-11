35138e394866c5fe5789b403b154f6a7bd0e6ceb
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-11 11:22:43 -0500

    v0.8.1 Se quito relacion org_sucursal en entidad factura

--- orm/fc_factura.php
+++ orm/fc_factura.php
@@ -11,11 +11,11 @@ class fc_factura extends modelo{
     public function __construct(PDO $link){
         $tabla = __CLASS__;
         $columnas = array($tabla=>false,'fc_cfd'=>$tabla, 'cat_sat_forma_pago'=>$tabla,'cat_sat_metodo_pago'=>$tabla,
-            'cat_sat_moneda'=>$tabla, 'com_tipo_cambio'=>$tabla, 'cat_sat_uso_cfdi'=>$tabla, 'org_sucursal'=>$tabla,
+            'cat_sat_moneda'=>$tabla, 'com_tipo_cambio'=>$tabla, 'cat_sat_uso_cfdi'=>$tabla,
             'cat_sat_tipo_de_comprobante'=>$tabla, 'dp_calle_pertenece'=>$tabla, 'cat_sat_regimen_fiscal'=>$tabla,
             'com_sucursal'=>$tabla);
         $campos_obligatorios = array('codigo', 'cat_sat_forma_pago_id','cat_sat_metodo_pago_id', 'cat_sat_moneda_id',
-            'com_tipo_cambio_id', 'cat_sat_uso_cfdi_id', 'org_sucursal_id', 'cat_sat_tipo_de_comprobante_id',
+            'com_tipo_cambio_id', 'cat_sat_uso_cfdi_id', 'cat_sat_tipo_de_comprobante_id',
             'dp_calle_pertenece_id', 'cat_sat_regimen_fiscal_id', 'com_sucursal_id');
 
         $no_duplicados = array('codigo','descripcion_select','alias','codigo_bis','serie');
