f3faa39c727a4441201f999f94c7e0b4169598ac
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-12 12:10:24 -0500

    v.0.16.1 Se agrego campos al CRUD de entidad fc_cfd_paritda

--- orm/base/limpieza.php
+++ orm/base/limpieza.php
@@ -1,8 +1,13 @@
 <?php
 namespace models\base;
 use base\controller\controler;
+use config\generales;
 use gamboamartin\errores\errores;
 use gamboamartin\validacion\validacion;
+use models\org_empresa;
+use models\org_sucursal;
+use models\org_tipo_sucursal;
+use PDO;
 use stdClass;
 
 class limpieza{
@@ -143,6 +148,59 @@ class limpieza{
         return $registro;
     }
 
+    /**
+     * Inicializa un registro previo al alta de sucursal desde empresa alta
+     * @param org_sucursal $modelo Modelo de org sucursal datos
+     * @return array
+     */
+    public function init_row_sucursal_alta(org_sucursal $modelo): array
+    {
+        $registro = $this->limpia_domicilio_con_calle(registro:$modelo->registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al limpiar sucursal', data: $registro);
+        }
+
+        if(isset($registro['guarda'])){
+            unset($registro['guarda']);
+        }
+
+        $org_tipo_sucursal_id =$this->row_tipo_sucursal_id(link:$modelo->link, org_sucursal: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al asignar tipo sucursal',data:  $org_tipo_sucursal_id);
+        }
+        $registro['org_tipo_sucursal_id'] = $org_tipo_sucursal_id;
+
+        if(!isset($registro['descripcion'])){
+            $org_empresa = (new org_empresa($modelo->link))->registro(registro_id: $registro['org_empresa_id']);
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al obtener empresa', data: $org_empresa);
+            }
+            $tipo_sucursal = (new org_tipo_sucursal($modelo->link))->registro(registro_id:$org_tipo_sucursal_id );
+
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al obtener tipo de sucursal', data: $tipo_sucursal);
+            }
+
+            $descripcion = $registro['codigo'].' '.$org_empresa['org_empresa_descripcion'].' ';
+            $descripcion .= $tipo_sucursal['org_tipo_sucursal_descripcion'];
+            $registro['descripcion'] = $descripcion;
+        }
+
+        $keys = array('descripcion');
+        $valida = $this->validacion->valida_existencia_keys(keys: $keys,registro:  $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar registro', data: $valida);
+        }
+
+        $registro['descripcion_select'] = strtoupper($registro['descripcion']);
+        $registro['alias'] = $registro['codigo'];
+
+
+        $modelo->registro = $registro;
+
+        return $modelo->registro;
+    }
+
     private function init_upd_org_empresa(controler $controler, stdClass $org_empresa): array|stdClass
     {
         $keys_foraneas = array('dp_pais_id','dp_estado_id','dp_municipio_id','dp_cp_id','dp_colonia_postal_id',
@@ -184,6 +242,23 @@ class limpieza{
         return $init;
     }
 
+    /**
+     * Limpia un row cuando este tiene calle sus parents
+     * @param array $registro registro a limpiar
+     * @return array
+     * @version 0.153.31
+     */
+    private function limpia_domicilio_con_calle(array $registro): array
+    {
+        $keys = array('dp_pais_id','dp_estado_id','dp_municipio_id','dp_cp_id','dp_colonia_postal_id');
+        foreach ($keys as $key){
+            if(isset($registro[$key])){
+                unset($registro[$key]);
+            }
+        }
+
+        return $registro;
+    }
 
 
     /**
@@ -213,5 +288,118 @@ class limpieza{
     }
 
 
+    /**
+     * Maqueta un arreglo con los datos para la insersion de una sucursal con datos de la empresa
+     * @param PDO $link Conexion a la base de datos
+     * @param int $org_empresa_id identificador
+     * @param array $org_empresa registro de tipo empresa
+     * @return array
+     */
+    public function org_sucursal_ins(PDO $link, int $org_empresa_id, array $org_empresa): array
+    {
+
+        $org_empresa_ = $org_empresa;
+        if(!isset($org_empresa_['codigo_bis'])){
+            $org_empresa_ = (new org_empresa($link))->registro(registro_id: $org_empresa_id,columnas_en_bruto: true);
+        }
+
+        $org_sucursal_ins['org_empresa_id'] = $org_empresa_id;
+        $org_sucursal_ins['codigo'] = $org_empresa_['codigo'];
+        $org_sucursal_ins['codigo_bis'] = $org_empresa_['codigo_bis'];
+        $org_sucursal_ins['descripcion'] = $org_empresa_['descripcion'];
+
+
+        if(isset($org_empresa_['fecha_inicio_operaciones'])){
+            $org_sucursal_ins['fecha_inicio_operaciones'] = $org_empresa_['fecha_inicio_operaciones'];
+        }
+        if(isset($org_empresa_['dp_calle_pertenece_id'])){
+            $org_sucursal_ins['dp_calle_pertenece_id'] = $org_empresa_['dp_calle_pertenece_id'];
+        }
+        if(isset($org_empresa_['telefono_1'])){
+            $org_sucursal_ins['telefono_1'] = $org_empresa_['telefono_1'];
+        }
+        if(isset($org_empresa_['telefono_2'])){
+            $org_sucursal_ins['telefono_2'] = $org_empresa_['telefono_2'];
+        }
+        if(isset($org_empresa_['telefono_3'])){
+            $org_sucursal_ins['telefono_3'] = $org_empresa_['telefono_3'];
+        }
+        if(isset($org_empresa_['exterior'])){
+            $org_sucursal_ins['exterior'] = $org_empresa_['exterior'];
+        }
+        if(isset($org_empresa_['interior'])){
+            $org_sucursal_ins['interior'] = $org_empresa_['interior'];
+        }
+
+        return $org_sucursal_ins;
+    }
+
+    private function row_tipo_sucursal_id(PDO $link, array $org_sucursal): array|int
+    {
+
+        $t_sucursal = $this->tipos_sucursal();
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener tipos de sucursal', data: $t_sucursal);
+        }
+
+        $org_tipo_sucursal_id = $this->tipo_sucursal_id(link: $link, org_sucursal: $org_sucursal,
+            t_sucursal: $t_sucursal);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener tipo sucursal',data:  $org_tipo_sucursal_id);
+        }
+        if(isset($org_sucursal['org_tipo_sucursal_id']) && (int)$org_sucursal['org_tipo_sucursal_id'] > 0){
+            $org_tipo_sucursal_id = $org_sucursal['org_tipo_sucursal_id'];
+        }
+
+        return  $org_tipo_sucursal_id;
+    }
+
+    private function tipos_sucursal(): stdClass
+    {
+        $generales = (new generales());
+        $tipo_sucursal_base_id = -1;
+        $tipo_sucursal_matriz_id = -1;
+        if(isset($generales->tipo_sucursal_base_id)){
+            $tipo_sucursal_base_id = $generales->tipo_sucursal_base_id;
+        }
+        if(isset($generales->tipo_sucursal_matriz_id)){
+            $tipo_sucursal_matriz_id = $generales->tipo_sucursal_matriz_id;
+        }
+
+        $data = new stdClass();
+        $data->tipo_sucursal_base_id = $tipo_sucursal_base_id;
+        $data->tipo_sucursal_matriz_id = $tipo_sucursal_matriz_id;
+        return $data;
+    }
+
+    private function tipo_sucursal_id(PDO $link, array $org_sucursal, stdClass $t_sucursal): array|int
+    {
+        $org_tipo_sucursal_id = -1;
+        $filtro = array();
+        $filtro['org_empresa.id'] = $org_sucursal['org_empresa_id'];
+        $n_sucursales = (new org_sucursal($link))->cuenta(filtro: $filtro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al contar sucursales',data:  $n_sucursales);
+        }
+        if((int)$n_sucursales === 0){
+            $org_tipo_sucursal_id = $t_sucursal->tipo_sucursal_matriz_id;
+        }
+        if((int)$n_sucursales >0){
+            $filtro = array();
+            $filtro['org_empresa.id'] = $org_sucursal['org_empresa_id'];
+            $filtro['org_tipo_sucursal.id'] = $t_sucursal->tipo_sucursal_matriz_id;
+            $n_sucursales = (new org_sucursal($link))->cuenta(filtro: $filtro);
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al contar sucursales',data:  $n_sucursales);
+            }
+            if((int)$n_sucursales === 0){
+                $org_tipo_sucursal_id = $t_sucursal->tipo_sucursal_matriz_id;
+            }
+            else{
+                $org_tipo_sucursal_id = $t_sucursal->tipo_sucursal_base_id;
+            }
+        }
+        return $org_tipo_sucursal_id;
+    }
 
 }
---
+++ templates/directivas/fc_cfd_partida_html.php
@@ -0,0 +1,20 @@
+<?php
+namespace html;
+
+
+use gamboamartin\errores\errores;
+use gamboamartin\facturacion\controllers\controlador_fc_cfd;
+use gamboamartin\organigrama\controllers\controlador_org_empresa;
+use gamboamartin\system\html_controler;
+
+use models\base\limpieza;
+use models\fc_cfd;
+use models\org_empresa;
+use PDO;
+use stdClass;
+
+
+class fc_cfd_partida_html extends html_controler {
+
+
+}
