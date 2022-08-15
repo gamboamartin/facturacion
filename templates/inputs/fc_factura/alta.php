99c2641e9d47d5132614afcab4e42b5b6f768a81
Angel Martínez De León <angelmartinezdeleon@gmail.com>
2022-08-15 16:52:46 -0500

    v.0.17.12 Se elimino campo moneda de alta de entidad fc_factura

--- templates/inputs/fc_factura/alta.php
+++ templates/inputs/fc_factura/alta.php
@@ -7,7 +7,6 @@
 <?php echo $controlador->inputs->select->cat_sat_tipo_de_comprobante_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_forma_pago_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_metodo_pago_id; ?>
-<?php echo $controlador->inputs->select->cat_sat_moneda_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_uso_cfdi_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_regimen_fiscal_id; ?>
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
