f63c167d50756cf1d37a1edec157529014479b3a
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-17 13:33:30 -0500

    0.21.14 Fix select factuyra alta

--- templates/inputs/fc_factura/alta.php
+++ templates/inputs/fc_factura/alta.php
@@ -4,7 +4,6 @@
 <?php echo $controlador->inputs->folio; ?>
 <?php echo $controlador->inputs->fecha; ?>
 <?php echo $controlador->inputs->exportacion; ?>
-<?php echo $controlador->inputs->select->com_sucursal_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_tipo_de_comprobante_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_forma_pago_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_metodo_pago_id; ?>
