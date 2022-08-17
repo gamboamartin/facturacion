199b7ecec805fdc490690952cc394d00f0dd5f51
Kevin <kevin.acuna@ivitec.com.mx>
2022-08-17 09:50:51 -0500

    v.0.19.13 Se integra param disabled en funciones inputs para entidad fc_factura

--- templates/directivas/fc_factura_html.php
+++ templates/directivas/fc_factura_html.php
@@ -118,14 +118,14 @@ class fc_factura_html extends html_controler {
         return $inputs;
     }
 
-    public function input_version(int $cols, stdClass $row_upd, bool $value_vacio): array|string
+    public function input_version(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false): array|string
     {
         $valida = $this->directivas->valida_cols(cols: $cols);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al validar columnas', data: $valida);
         }
 
-        $html =$this->directivas->input_text_required(disable: false,name: 'version',place_holder: 'Version',
+        $html =$this->directivas->input_text_required(disable: $disabled,name: 'version',place_holder: 'Version',
             row_upd: $row_upd, value_vacio: $value_vacio);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar input', data: $html);
@@ -139,14 +139,14 @@ class fc_factura_html extends html_controler {
         return $div;
     }
 
-    public function input_serie(int $cols, stdClass $row_upd, bool $value_vacio): array|string
+    public function input_serie(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false): array|string
     {
         $valida = $this->directivas->valida_cols(cols: $cols);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al validar columnas', data: $valida);
         }
 
-        $html =$this->directivas->input_text_required(disable: false,name: 'serie',place_holder: 'Serie',
+        $html =$this->directivas->input_text_required(disable: $disabled,name: 'serie',place_holder: 'Serie',
             row_upd: $row_upd, value_vacio: $value_vacio);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar input', data: $html);
@@ -160,14 +160,14 @@ class fc_factura_html extends html_controler {
         return $div;
     }
 
-    public function input_folio(int $cols, stdClass $row_upd, bool $value_vacio): array|string
+    public function input_folio(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false): array|string
     {
         $valida = $this->directivas->valida_cols(cols: $cols);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al validar columnas', data: $valida);
         }
 
-        $html =$this->directivas->input_text_required(disable: false,name: 'folio',place_holder: 'Folio',
+        $html =$this->directivas->input_text_required(disable: $disabled,name: 'folio',place_holder: 'Folio',
             row_upd: $row_upd, value_vacio: $value_vacio);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar input', data: $html);
@@ -181,14 +181,14 @@ class fc_factura_html extends html_controler {
         return $div;
     }
 
-    public function input_fecha(int $cols, stdClass $row_upd, bool $value_vacio): array|string
+    public function input_fecha(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false): array|string
     {
         $valida = $this->directivas->valida_cols(cols: $cols);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al validar columnas', data: $valida);
         }
 
-        $html =$this->directivas->fecha_required(disable: false,name: 'fecha',place_holder: 'Fecha',
+        $html =$this->directivas->fecha_required(disable: $disabled,name: 'fecha',place_holder: 'Fecha',
             row_upd: $row_upd, value_vacio: $value_vacio);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar input', data: $html);
@@ -202,14 +202,14 @@ class fc_factura_html extends html_controler {
         return $div;
     }
 
-    public function input_exportacion(int $cols, stdClass $row_upd, bool $value_vacio): array|string
+    public function input_exportacion(int $cols, stdClass $row_upd, bool $value_vacio, bool $disabled = false): array|string
     {
         $valida = $this->directivas->valida_cols(cols: $cols);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al validar columnas', data: $valida);
         }
 
-        $html =$this->directivas->input_text_required(disable: false,name: 'exportacion',place_holder: 'Exportacion',
+        $html =$this->directivas->input_text_required(disable: $disabled,name: 'exportacion',place_holder: 'Exportacion',
             row_upd: $row_upd, value_vacio: $value_vacio);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar input', data: $html);
