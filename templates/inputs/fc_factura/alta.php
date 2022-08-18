f5c93058cbbc90368bb46826cbaf417c0dadccfe
angel.martinez@ivitec.com.mx <angel.martinez@ivitec.com.mx>
2022-08-18 15:40:12 -0500

    0.31.14 Se agrego select moneda en entidad fc_factura

--- templates/inputs/fc_factura/alta.php
+++ templates/inputs/fc_factura/alta.php
@@ -7,13 +7,12 @@
 <?php echo $controlador->inputs->exportacion; ?>
 <?php echo $controlador->inputs->version; ?>
 <?php echo $controlador->inputs->serie; ?>
-
 <?php echo $controlador->inputs->subtotal; ?>
 <?php echo $controlador->inputs->descuento; ?>
 <?php echo $controlador->inputs->total; ?>
-
 <?php echo $controlador->inputs->select->cat_sat_tipo_de_comprobante_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_forma_pago_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_metodo_pago_id; ?>
+<?php echo $controlador->inputs->select->cat_sat_moneda_id; ?>
 <?php echo $controlador->inputs->select->cat_sat_uso_cfdi_id; ?>
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
