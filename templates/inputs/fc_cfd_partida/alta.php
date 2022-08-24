f8295d319625f9f5757407cee4a912c15ff32213
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-08-24 14:13:01 -0500

    v0.45.15 Se integra vista de alta de entidad partida

--- templates/inputs/fc_cfd_partida/alta.php
+++ templates/inputs/fc_cfd_partida/alta.php
@@ -1,2 +1,10 @@
 <?php /** @var  \gamboamartin\facturacion\controllers\controlador_fc_cfd_partida $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
+<?php echo $controlador->inputs->select->fc_factura_id; ?>
+<?php echo $controlador->inputs->select->com_sucursal_id; ?>
+<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->descripcion; ?>
+<?php echo $controlador->inputs->cantidad; ?>
+<?php echo $controlador->inputs->valor_unitario; ?>
+<?php echo $controlador->inputs->descuento; ?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
