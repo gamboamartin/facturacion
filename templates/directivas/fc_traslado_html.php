9bd275d0d8f414bf38d8899b6920a2a14b6caca8
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-08 13:01:18 -0500

    v0.96.26 Se integra CRUD fc_traslado campos base

---
+++ templates/directivas/fc_traslado_html.php
@@ -0,0 +1,32 @@
+<?php
+namespace html;
+
+
+use gamboamartin\errores\errores;
+use gamboamartin\facturacion\controllers\controlador_fc_cer_csd;
+use gamboamartin\organigrama\controllers\controlador_org_empresa;
+use gamboamartin\system\html_controler;
+
+use models\base\limpieza;
+use models\fc_cer_csd;
+use models\org_empresa;
+use PDO;
+use stdClass;
+
+
+class fc_traslado_html extends html_controler {
+
+    public function select_fc_traslado_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+                                          bool $required = false): array|string
+    {
+        $modelo = new fc_cer_csd($link);
+
+        $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
+            modelo: $modelo, label: "Traslado", required: $required);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al generar select', data: $select);
+        }
+        return $select;
+    }
+
+}
