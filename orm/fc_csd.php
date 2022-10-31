de55ca801d037cf1a21c4a5f0124c2612e3db37b
Kevin <kevin.acuna@ivitec.com.mx>
2022-10-31 18:41:32 -0500

    v0.138.0 Se ajustan views en fc_csd

--- orm/fc_csd.php
+++ orm/fc_csd.php
@@ -1,8 +1,10 @@
 <?php
 namespace gamboamartin\facturacion\models;
 use base\orm\modelo;
+use gamboamartin\errores\errores;
 use gamboamartin\organigrama\models\org_sucursal;
 use PDO;
+use stdClass;
 
 
 class fc_csd extends modelo{
@@ -14,14 +16,69 @@ class fc_csd extends modelo{
 
         $no_duplicados = array('codigo','descripcion_select','alias','codigo_bis','serie');
 
-        $campos_view = array();
-        $campos_view['org_sucursal_id']['type'] = 'selects';
-        $campos_view['org_sucursal_id']['model'] = (new org_sucursal($link));
+        $campos_view['org_sucursal_id'] = array('type' => 'selects', 'model' => new org_sucursal($link));
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['codigo_bis'] = array('type' => 'inputs');
+        $campos_view['serie'] = array('type' => 'inputs');
 
         $campos_view['serie']['type'] = 'inputs';
 
-        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas,no_duplicados: $no_duplicados,tipo_campos: array(), campos_view: $campos_view);
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, campos_view: $campos_view, no_duplicados: $no_duplicados, tipo_campos: array());
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function alta_bd(): array|stdClass
+    {
+        if(!isset($this->registro['codigo_bis'])){
+            $this->registro['codigo_bis'] = $this->registro['codigo'];
+        }
+
+        if(!isset($this->registro['descripcion'])){
+            $this->registro['descripcion'] = $this->registro['codigo'];
+        }
+
+        if(!isset($this->registro['descripcion_select'])){
+            $this->registro['descripcion_select'] = $this->registro['codigo'];
+        }
+
+        if(!isset($this->registro['alias'])){
+            $this->registro['alias'] = $this->registro['codigo'];
+        }
+
+        $r_alta_bd = parent::alta_bd();
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al dar de alta csd',data: $r_alta_bd);
+        }
+
+        return $r_alta_bd;
+    }
+
+    public function modifica_bd(array $registro, int $id, bool $reactiva = false): array|stdClass
+    {
+        if(!isset($this->registro['codigo_bis'])){
+            $this->registro['codigo_bis'] = $this->registro['codigo'];
+        }
+
+        if(!isset($this->registro['descripcion'])){
+            $this->registro['descripcion'] = $this->registro['codigo'];
+        }
+
+        if(!isset($this->registro['descripcion_select'])){
+            $this->registro['descripcion_select'] = $this->registro['codigo'];
+        }
+
+        if(!isset($this->registro['alias'])){
+            $this->registro['alias'] = $this->registro['codigo'];
+        }
+
+        $r_modifica_bd = parent::modifica_bd($registro, $id, $reactiva);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al modificar csd',data: $r_modifica_bd);
+        }
+
+        return $r_modifica_bd;
     }
 
 }
