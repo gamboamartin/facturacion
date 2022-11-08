7f3c09ac285651319dc3682ed4b503fcfe2e2b95
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-08 18:23:38 -0500

    v0.153.0 Se ajusta alta key csd

--- views/fc_key_csd/alta.php
+++ views/fc_key_csd/alta.php
@@ -13,7 +13,7 @@
 
                         <?php echo $controlador->inputs->codigo; ?>
                         <?php echo $controlador->inputs->codigo_bis; ?>
-                        c
+                        <?php echo $controlador->inputs->documento; ?>
                         <?php echo $controlador->inputs->fc_csd_id; ?>
                         <?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
                     </form>
