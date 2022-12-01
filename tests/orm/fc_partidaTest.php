300cd9b78ec584eee528d9b8f541bab5eb28c013
mgamboa <gamboa.martin@gmail.com>
2022-12-01 14:59:30 -0600

    v0.179.0 Se ajustan ut

--- tests/orm/fc_partidaTest.php
+++ tests/orm/fc_partidaTest.php
@@ -22,45 +22,7 @@ class fc_partidaTest extends test {
         $this->paths_conf->views = '/var/www/html/cat_sat/config/views.php';
     }
 
-    public function test_valida_partida_alta(): void
-    {
-        errores::$error = false;
-
-        $_GET['seccion'] = 'cat_sat_tipo_persona';
-        $_GET['accion'] = 'lista';
-        $_SESSION['grupo_id'] = 1;
-        $_SESSION['usuario_id'] = 2;
-        $_GET['session_id'] = '1';
-
-        $modelo = new fc_partida($this->link);
-        $modelo = new liberator($modelo);
-
-        $registro = array();
-        $resultado = $modelo->valida_partida_alta($registro);
-
-        $this->assertIsArray($resultado);
-        $this->assertTrue(errores::$error);
-        $this->assertStringContainsStringIgnoringCase('Error al validar registro', $resultado['mensaje']);
-        errores::$error = false;
-
-        $registro = array();
-        $registro['fc_factura_id'] = 1;
-        $resultado = $modelo->valida_partida_alta($registro);
-        $this->assertIsArray($resultado);
-        $this->assertTrue(errores::$error);
-        $this->assertStringContainsStringIgnoringCase('Error al validar registro', $resultado['mensaje']);
-        errores::$error = false;
-
-
-        $registro = array();
-        $registro['fc_factura_id'] = 1;
-        $registro['com_producto_id'] = 1;
-        $resultado = $modelo->valida_partida_alta($registro);
-        $this->assertIsBool($resultado);
-        $this->assertNotTrue(errores::$error);
-
-        errores::$error = false;
-    }
+    
 
 
 }
