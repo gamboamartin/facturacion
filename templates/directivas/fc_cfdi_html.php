991c3e49569b93451889ba4cd1ffe666192cb643
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-12-06 12:26:32 -0600

    v0.193.0 Se integra entidad cfdi

---
+++ templates/directivas/fc_cfdi_html.php
@@ -0,0 +1,17 @@
+<?php
+namespace html;
+
+
+use gamboamartin\errores\errores;
+use gamboamartin\facturacion\controllers\controlador_fc_cfdi;
+use gamboamartin\system\html_controler;
+
+use PDO;
+use stdClass;
+
+
+class fc_cfdi_html extends html_controler {
+
+
+
+}
