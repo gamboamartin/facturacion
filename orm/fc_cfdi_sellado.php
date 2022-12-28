e0aa0fcc5f484fe1327592440b3346e9745c3cb0
Kevin <kevin.acuna@ivitec.com.mx>
2022-12-28 18:42:51 -0500

    v1.5.0 Se integra cfdi sellado

---
+++ orm/fc_cfdi_sellado.php
@@ -0,0 +1,90 @@
+<?php
+namespace gamboamartin\facturacion\models;
+use base\orm\_modelo_parent;
+use gamboamartin\errores\errores;
+use PDO;
+use stdClass;
+
+class fc_cfdi_sellado extends _modelo_parent {
+    public function __construct(PDO $link){
+        $tabla = 'fc_cfdi_sellado';
+        $columnas = array($tabla=>false, 'fc_factura' => $tabla);
+
+        $campos_obligatorios = array();
+
+        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas);
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        if(!isset($this->registro['codigo'])){
+            $this->registro['codigo'] =  $this->registro['comprobante_no_certificado'];
+        }
+
+        $this->registro = $this->campos_base(data: $this->registro,modelo: $this);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar campos base',data: $this->registro);
+        }
+
+        $this->registro = $this->validaciones(data: $this->registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar foraneas',data: $this->registro);
+        }
+
+        $r_alta_bd =  parent::alta_bd();
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error registrar cfdi sellado', data: $r_alta_bd);
+        }
+
+        return $r_alta_bd;
+    }
+
+
+    public function modifica_bd(array $registro, int $id, bool $reactiva = false,
+                                array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        if(!isset($registro['codigo'])){
+            $registro['codigo'] =  $registro["comprobante_no_certificado"];
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al obtener el codigo del registro',data: $registro);
+            }
+        }
+
+        $registro = $this->campos_base(data: $registro,modelo: $this,id: $id);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar campos base',data: $registro);
+        }
+
+        $registro = $this->validaciones(data: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar foraneas',data: $registro);
+        }
+
+        $r_modifica_bd = parent::modifica_bd($registro, $id, $reactiva);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al modificar cfdi sellado',data:  $r_modifica_bd);
+        }
+
+        return $r_modifica_bd;
+    }
+
+    private function validaciones(array $data): array
+    {
+        $keys = array('descripcion','codigo');
+        $valida = $this->validacion->valida_existencia_keys(keys:$keys,registro:  $data);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar campos', data: $valida);
+        }
+
+        $keys = array('fc_factura_id');
+        $valida = $this->validacion->valida_ids(keys: $keys, registro: $data);
+        if(errores::$error){
+            return $this->error->error(mensaje: "Error al validar foraneas",data:  $valida);
+        }
+
+        return $data;
+    }
+}
