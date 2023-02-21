c2674656760ceba0e903fa18bfaeed085e809d0e
mgamboa <gamboa.martin@gmail.com>
2023-02-21 17:24:06 -0600

    v4.27.0 Se ajusta alta fc

--- templates/inputs/fc_factura/alta.php
+++ templates/inputs/fc_factura/alta.php
@@ -14,11 +14,4 @@
 <?php echo $controlador->inputs->cat_sat_uso_cfdi_id; ?>
 <?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
 
-<div class="col-md-12">
-    <?php
-    foreach ($controlador->buttons_parents_alta as $button){ ?>
-        <div class="col-md-4">
-            <?php echo $button; ?>
-        </div>
-    <?php } ?>
-</div>
+
