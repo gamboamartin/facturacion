adad3849ad3880fe03e77cf52136b8b4d97d6104
mgamboa <gamboa.martin@gmail.com>
2023-03-03 10:55:15 -0600

    v4.38.0 Se integra moneda in pais fc

--- controllers/controlador_dp_pais.php
+++ controllers/controlador_dp_pais.php
@@ -8,4 +8,13 @@
  */
 namespace gamboamartin\facturacion\controllers;
 
-class controlador_dp_pais extends \controllers\controlador_dp_pais {}
+use PDO;
+use stdClass;
+
+class controlador_dp_pais extends \controllers\controlador_dp_pais {
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass())
+    {
+        parent::__construct(link: $link, paths_conf: $paths_conf);
+        $this->childrens_data['cat_sat_moneda']['title'] = 'Moneda';
+    }
+}
