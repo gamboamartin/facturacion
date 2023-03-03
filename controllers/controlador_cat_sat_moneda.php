2da2a4849395e95a18d6cc9398c7595937824e19
mgamboa <gamboa.martin@gmail.com>
2023-03-03 11:10:06 -0600

    v4.39.0 Se integra moneda to client

--- controllers/controlador_cat_sat_moneda.php
+++ controllers/controlador_cat_sat_moneda.php
@@ -8,4 +8,15 @@
  */
 namespace gamboamartin\facturacion\controllers;
 
-class controlador_cat_sat_moneda extends \gamboamartin\comercial\controllers\controlador_cat_sat_moneda {}
+use PDO;
+use stdClass;
+
+class controlador_cat_sat_moneda extends \gamboamartin\comercial\controllers\controlador_cat_sat_moneda {
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass())
+    {
+        parent::__construct(link: $link,paths_conf:  $paths_conf);
+
+        $this->childrens_data['com_cliente']['title'] = 'Cliente';
+
+    }
+}
