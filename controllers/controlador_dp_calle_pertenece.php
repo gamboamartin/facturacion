a270a12102f68db705e0d01de7de7c846cdf3b2a
mgamboa <gamboa.martin@gmail.com>
2023-03-03 09:40:18 -0600

    v4.35.0 Se integra child in uso cfdi

--- controllers/controlador_dp_calle_pertenece.php
+++ controllers/controlador_dp_calle_pertenece.php
@@ -8,4 +8,15 @@
  */
 namespace gamboamartin\facturacion\controllers;
 
-class controlador_dp_calle_pertenece extends \controllers\controlador_dp_calle_pertenece {}
+use PDO;
+use stdClass;
+
+class controlador_dp_calle_pertenece extends \controllers\controlador_dp_calle_pertenece {
+    public function __construct(PDO $link, stdClass $paths_conf = new stdClass())
+    {
+        parent::__construct(link: $link,paths_conf:  $paths_conf);
+        $this->childrens_data['org_sucursal']['title'] = 'Sucursal';
+        $this->childrens_data['org_empresa']['title'] = 'Empresa';
+    }
+
+}
