d74579c1e9748e75b2c650e1cad2cf3198771510
mgamboa <gamboa.martin@gmail.com>
2023-03-15 14:12:20 -0600

    v5.4.1 Se integra ajuste de heredacion

--- controllers/controlador_adm_grupo.php
+++ controllers/controlador_adm_grupo.php
@@ -5,12 +5,8 @@ use gamboamartin\template_1\html;
 use PDO;
 use stdClass;
 
-class controlador_adm_grupo extends \gamboamartin\documento\controllers\controlador_adm_grupo {
+class controlador_adm_grupo extends \gamboamartin\acl\controllers\controlador_adm_grupo {
 
-    public function __construct(PDO $link, html $html = new html(), stdClass $paths_conf = new stdClass())
-    {
-        parent::__construct($link, $html, $paths_conf);
-        $this->path_vendor_views = 'gamboa.martin/documento';
-    }
+
 
 }
