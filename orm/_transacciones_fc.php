0194ec6a01ac3cb01dc4cf5098b65b49afcd71b4
mgamboa <gamboa.martin@gmail.com>
2023-06-02 12:56:02 -0600

    v10.26.0 Se integra data cfdi

--- orm/_transacciones_fc.php
+++ orm/_transacciones_fc.php
@@ -721,6 +721,9 @@ class _transacciones_fc extends modelo
         if (errores::$error) {
             return $this->error->error(mensaje: 'Error al obtener datos de la factura', data: $data_factura);
         }
+        if(!isset($data_factura->Complemento)){
+            $data_factura->Complemento = array();
+        }
 
 
         if($tipo === 'xml') {
