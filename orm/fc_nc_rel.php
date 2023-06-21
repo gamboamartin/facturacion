22c86bc4af77afed9cee8937162b193daf78b985
mgamboa <gamboa.martin@gmail.com>
2023-06-21 09:24:44 -0600

    v10.102.3 Se integra doc in row upd

--- orm/fc_nc_rel.php
+++ orm/fc_nc_rel.php
@@ -65,7 +65,7 @@ class fc_nc_rel extends _modelo_parent {
             return $this->error->error(mensaje: 'Error al insertar',data:  $r_alta_bd);
         }
 
-        $regenera = (new fc_factura(link: $this->link))->regenera_saldos(fc_factura_id:  $this->registro['fc_factura_id']);
+        $regenera = (new _saldos_fc())->regenera_saldos(fc_factura_id:  $this->registro['fc_factura_id'], link: $this->link);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al actualizar monto regenera',data:  $regenera);
         }
