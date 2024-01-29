ce47228a442f5f6fc68ee41ecf62c71d0d083184
mgamboa <gamboa.martin@gmail.com>
2024-01-29 16:05:58 -0600

    v23.4.0 Se integran modelo heradados

--- controllers/controlador_com_cliente.php
+++ controllers/controlador_com_cliente.php
@@ -8,4 +8,19 @@
  */
 namespace gamboamartin\facturacion\controllers;
 
-class controlador_com_cliente extends \gamboamartin\comercial\controllers\controlador_com_cliente {}
+use gamboamartin\facturacion\models\com_cliente;
+use gamboamartin\template\html;
+use PDO;
+use stdClass;
+
+class controlador_com_cliente extends \gamboamartin\comercial\controllers\controlador_com_cliente {
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass())
+    {
+        parent::__construct(link: $link,html:  $html,paths_conf:  $paths_conf);
+
+        $this->modelo = new com_cliente(link: $this->link);
+
+
+    }
+}
