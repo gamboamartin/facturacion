f75ea73a5ea6f7a95968143624a2fea0b769d14a
mgamboa <gamboa.martin@gmail.com>
2024-03-08 11:28:09 -0600

    v27.61.0 Se ajusta ins descricpion proceso

--- instalacion/instalacion.php
+++ instalacion/instalacion.php
@@ -2857,17 +2857,11 @@ class instalacion
         }
         if($existe){
 
-            $r_pr_proceso = (new pr_proceso(link: $link))->filtro_and(filtro: $filtro);
+            $r_pr_proceso = (new pr_proceso(link: $link))->elimina_con_filtro_and(filtro: $filtro);
             if(errores::$error){
                 return (new errores())->error(mensaje: 'Error al obtener proceso',data:  $r_pr_proceso);
             }
 
-            $upd = array();
-            $upd['descripcion'] = 'FACTURACION';
-            $r_upd = (new pr_proceso(link: $link))->modifica_bd(registro: $upd,id:  $r_pr_proceso->registros[0]['pr_proceso_id']);
-            if(errores::$error){
-                return (new errores())->error(mensaje: 'Error al actualizar proceso',data:  $r_upd);
-            }
         }
 
         $inserta = $this->genera_pr_etapa_proceso(adm_accion_descripcion: 'alta_bd',adm_seccion_descripcion: __FUNCTION__,
