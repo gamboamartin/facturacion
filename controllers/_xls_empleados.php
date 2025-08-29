bcfb310a5c673aeeb03976ebe722d52c14378f7c
corporativoem3 <desarrollo@corporativoem3.com.mx>
2025-08-29 17:36:07 -0600

    v35.78.1 eMPLEADOS

---
+++ controllers/_xls_empleados.php
@@ -0,0 +1,208 @@
+<?php
+namespace gamboamartin\facturacion\controllers;
+
+use gamboamartin\errores\errores;
+use gamboamartin\facturacion\models\fc_empleado;
+use gamboamartin\facturacion\models\fc_layout_nom;
+use PDO;
+use stdClass;
+
+class _xls_empleados{
+
+    private function alta_empleado(array $row_empleado, PDO $link): array|stdClass
+    {
+        $fc_empleado_modelo = new fc_empleado($link);
+
+        $fc_empleado_new = $this->fc_empleado_new($row_empleado);
+        if(errores::$error){
+            return (new errores())->error('Error al generar row', $fc_empleado_new);
+        }
+        $alta_em = $fc_empleado_modelo->alta_registro($fc_empleado_new);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al insertar $empleado', data: $alta_em);
+        }
+        return $alta_em;
+
+    }
+
+    private function asigna_value_row(string $key, array $row_empleado, mixed $value): array
+    {
+        if(is_null($value)){
+            $value = '';
+        }
+        $value = trim($value);
+        $value = strtoupper($value);
+        $key_row_emp = _xls_dispersion::$letras[$key];
+        $row_empleado[$key_row_emp] = trim($value);
+        return $row_empleado;
+
+    }
+
+    final public function carga_empleados(PDO $link, int $fc_layout_nom_id): array
+    {
+        $fc_layout_nom = (new fc_layout_nom($link))->registro($fc_layout_nom_id,retorno_obj: true);
+        if (errores::$error) {
+            return (new errores())->error(mensaje: 'Error al obtener layout', data: $fc_layout_nom);
+        }
+
+
+        $rows_empleados = $this->verifica_empleados($fc_layout_nom,$link);
+        if(errores::$error){
+            return (new errores())->error('Error al generar row', $rows_empleados);
+        }
+
+        return $rows_empleados;
+
+    }
+
+    private function fc_empleado_new(array $row_empleado): array
+    {
+        $fc_empleado_new = array();
+
+        $fc_empleado_new['nombre_completo'] = $row_empleado['NOMBRE COMPLETO'];
+        $fc_empleado_new['rfc'] = $row_empleado['RFC'];
+        $fc_empleado_new['cp'] = $row_empleado['CODIGO POSTAL'];
+        $fc_empleado_new['regimen_fiscal'] = '';
+        $fc_empleado_new['clabe'] = $row_empleado['CLABE INTERBANCARIA'];
+        $fc_empleado_new['nss'] = $row_empleado['NSS'];
+        $fc_empleado_new['curp'] = $row_empleado['CURP'];
+        $fc_empleado_new['validado_sat'] = 'inactivo';
+
+        return $fc_empleado_new;
+
+    }
+
+    private function genera_row_emp_val(stdClass $datos, int $recorrido): array
+    {
+        $row_empleado = $this->row_empleado($datos, $recorrido);
+        if(errores::$error){
+            return (new errores())->error('Error al generar row', $row_empleado);
+        }
+
+        $row_emp_val = $this->row_emp_val($datos, $row_empleado);
+        if(errores::$error){
+            return (new errores())->error('Error al generar row', $row_emp_val);
+        }
+
+        return $row_emp_val;
+
+    }
+
+    private function get_empleados_by_rfc(array $row_empleado, PDO $link): array|stdClass
+    {
+        $fc_empleado_modelo = new fc_empleado($link);
+        $rfc = trim($row_empleado['RFC']);
+        $sql = "SELECT *FROM fc_empleado WHERE rfc = '$rfc'";
+        $fc_empleados =  $fc_empleado_modelo->ejecuta_consulta($sql);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $datos', data: $fc_empleados);
+        }
+        return $fc_empleados;
+
+    }
+
+    private function init_row_cp(array$row_empleado): array
+    {
+        if(!isset($row_empleado['CODIGO POSTAL'])){
+            $row_empleado['CODIGO POSTAL'] = '';
+        }
+        if($row_empleado['CODIGO POSTAL'] === ''){
+            $row_empleado['CODIGO POSTAL'] = 'SIN CP';
+        }
+        return $row_empleado;
+
+    }
+
+    private function row_emp_val(stdClass $datos, array $row_empleado): array
+    {
+        $row_emp_val = array();
+        foreach ($row_empleado as $letra=>$value){
+            if(isset($datos->columnas[$letra])) {
+                $tag = $datos->columnas[$letra];
+                $row_emp_val[$tag] = trim($value);
+            }
+        }
+        return $row_emp_val;
+
+    }
+
+    private function row_empleado(stdClass $datos, int $recorrido): array
+    {
+        $valores_fila = $datos->hoja->rangeToArray($datos->primer_columna.$recorrido.':'.$datos->ultima_columna.$recorrido, null, true, false);
+        $row_xls = $valores_fila[0];
+
+        $row_empleado = array();
+        foreach ($row_xls as $key=>$value){
+            $row_empleado = $this->asigna_value_row($key,$row_empleado,$value);
+            if(errores::$error){
+                return (new errores())->error('Error al generar row', $row_empleado);
+            }
+        }
+
+        return $row_empleado;
+
+    }
+
+    private function row_empleados(stdClass $datos): array
+    {
+        $recorrido = $datos->fila_inicial;
+        $rows_empleados = array();
+        while ($recorrido <= $datos->ultima_fila) {
+            $row_emp_val = $this->genera_row_emp_val($datos, $recorrido);
+            if(errores::$error){
+                return (new errores())->error('Error al generar row', $row_emp_val);
+            }
+            $rows_empleados[] = $row_emp_val;
+            $recorrido++;
+        }
+
+        return $rows_empleados;
+
+    }
+
+    private function transacciona_empleado(array $row_empleado, PDO $link): array|stdClass
+    {
+        $row_empleado = $this->init_row_cp($row_empleado);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $datos', data: $row_empleado);
+        }
+
+        $fc_empleados =  $this->get_empleados_by_rfc($row_empleado,$link);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $datos', data: $fc_empleados);
+        }
+
+        if((int)$fc_empleados->n_registros === 0){
+            $alta_em = $this->alta_empleado($row_empleado,$link);
+            if(errores::$error){
+                return (new errores())->error(mensaje: 'Error al insertar $empleado', data: $alta_em);
+            }
+        }
+        return $fc_empleados;
+    }
+
+    private function verifica_empleados(stdClass $fc_layout_nom, PDO $link): array
+    {
+        $datos = (new _xls_dispersion())->lee_layout_base(fc_layout_nom: $fc_layout_nom);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $datos', data: $datos);
+        }
+
+        $rows_empleados = $this->row_empleados($datos);
+        if(errores::$error){
+            return (new errores())->error('Error al generar row', $rows_empleados);
+        }
+
+        foreach ($rows_empleados as $row_empleado){
+            $alta_em = $this->transacciona_empleado($row_empleado, $link);
+            if(errores::$error){
+                return (new errores())->error(mensaje: 'Error al insertar $empleado', data: $alta_em);
+            }
+        }
+
+        return $rows_empleados;
+    }
+
+
+
+}
--- controllers/controlador_fc_layout_nom.php
+++ controllers/controlador_fc_layout_nom.php
@@ -79,95 +79,21 @@ class controlador_fc_layout_nom extends system{
 
     }
 
+
+
     public function carga_empleados(bool $header, bool $ws = false)
     {
-        $fc_layout_nom = (new fc_layout_nom($this->link))->registro($this->registro_id,retorno_obj: true);
-        if (errores::$error) {
-            return $this->retorno_error(
-                mensaje: 'Error al obtener layout', data: $fc_layout_nom, header: $header, ws: $ws);
-        }
 
-        $datos = (new _xls_dispersion())->lee_layout_base(fc_layout_nom: $fc_layout_nom);
+        $rows_empleados = (new _xls_empleados())->carga_empleados($this->link,$this->registro_id);
         if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al obtener $datos', data: $datos,
-                header: $header, ws: $ws);
+            return (new errores())->error('Error al generar row', $rows_empleados);
         }
-
-
-        $recorrido = $datos->fila_inicial;
-        $rows_empleados = array();
-        while ($recorrido <= $datos->ultima_fila) {
-            $valores_fila = $datos->hoja->rangeToArray($datos->primer_columna.$recorrido.':'.$datos->ultima_columna.$recorrido, null, true, false);
-            $row_xls = $valores_fila[0];
-            $row_empleado = array();
-            foreach ($row_xls as $key=>$value){
-                if(is_null($value)){
-                    $value = '';
-                }
-                $value = trim($value);
-                $value = strtoupper($value);
-                $row_empleado[_xls_dispersion::$letras[$key]] = trim($value);
-            }
-
-            $row_emp_val = array();
-            foreach ($row_empleado as $letra=>$value){
-                if(isset($datos->columnas[$letra])) {
-                    $tag = $datos->columnas[$letra];
-                    $row_emp_val[$tag] = trim($value);
-                }
-            }
-            $rows_empleados[] = $row_emp_val;
-            $recorrido++;
-        }
-
-
-        foreach ($rows_empleados as $row_empleado){
-           $rfc = trim($row_empleado['RFC']);
-
-            if(!isset($row_empleado['CODIGO POSTAL'])){
-                $row_empleado['CODIGO POSTAL'] = '';
-            }
-
-           if($row_empleado['CODIGO POSTAL'] === ''){
-               $row_empleado['CODIGO POSTAL'] = 'SIN CP';
-           }
-
-           $sql = "SELECT *FROM fc_empleado WHERE rfc = '$rfc'";
-           $fc_empleados =  $this->modelo->ejecuta_consulta($sql);
-           if(errores::$error){
-               return $this->retorno_error(mensaje: 'Error al obtener $datos', data: $fc_empleados,
-                   header: $header, ws: $ws);
-           }
-           $fc_empleado_modelo = new fc_empleado($this->link);
-           if((int)$fc_empleados->n_registros === 0){
-
-               $fc_empleado_new = array();
-
-               $fc_empleado_new['nombre_completo'] = $row_empleado['NOMBRE COMPLETO'];
-               $fc_empleado_new['rfc'] = $row_empleado['RFC'];
-               $fc_empleado_new['cp'] = $row_empleado['CODIGO POSTAL'];
-               $fc_empleado_new['regimen_fiscal'] = '';
-               $fc_empleado_new['clabe'] = $row_empleado['CLABE INTERBANCARIA'];
-               $fc_empleado_new['nss'] = $row_empleado['NSS'];
-               $fc_empleado_new['curp'] = $row_empleado['CURP'];
-               $fc_empleado_new['validado_sat'] = 'inactivo';
-
-               $alta_em = $fc_empleado_modelo->alta_registro($fc_empleado_new);
-               if(errores::$error){
-                   return $this->retorno_error(mensaje: 'Error al insertar $empleado', data: $alta_em,
-                       header: $header, ws: $ws);
-               }
-           }
-
-        }
-
-
-        print_r($rows_empleados);exit;
 
         exit;
 
 
     }
+
 
     public function genera_dispersion(bool $header, bool $ws = false)
     {
@@ -175,6 +101,12 @@ class controlador_fc_layout_nom extends system{
         if (errores::$error) {
             return $this->retorno_error(
                 mensaje: 'Error al obtener layout', data: $fc_layout_nom, header: $header, ws: $ws);
+        }
+
+        $verif_empleado = (new _xls_empleados())->carga_empleados($this->link,$this->registro_id);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al verificar empleado', data: $verif_empleado,
+                header: $header, ws: $ws);
         }
 
         $datos = (new _xls_dispersion())->lee_layout_base(fc_layout_nom: $fc_layout_nom);
@@ -188,6 +120,7 @@ class controlador_fc_layout_nom extends system{
             return $this->retorno_error(mensaje: 'Error al generar $layout_dispersion', data: $xls,
                 header: $header, ws: $ws);
         }
+
         exit;
 
 
