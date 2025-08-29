dc4a732296b873cdf57602cc5a26b812e1079e83
corporativoem3 <desarrollo@corporativoem3.com.mx>
2025-08-29 15:08:10 -0600

    v35.75.1 Se louyout

---
+++ controllers/_xls_dispersion.php
@@ -0,0 +1,400 @@
+<?php
+namespace gamboamartin\facturacion\controllers;
+
+use gamboamartin\errores\errores;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use stdClass;
+use Throwable;
+
+class _xls_dispersion{
+
+    private function autosize(Worksheet $hoja): Worksheet
+    {
+        $hoja->getColumnDimension('A')->setAutoSize(true);
+        $hoja->getColumnDimension('B')->setAutoSize(true);
+        $hoja->getColumnDimension('C')->setAutoSize(true);
+        $hoja->getColumnDimension('D')->setAutoSize(true);
+
+        return $hoja;
+
+    }
+
+    private function carga_row_layout(Worksheet $hoja, array $layout_dispersion, int $recorrido): array
+    {
+        $valores_fila = $this->init_valores_fila(hoja: $hoja,recorrido:  $recorrido);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $valores_fila', data: $valores_fila);
+        }
+        $row = $this->genera_row_fila($valores_fila);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $row', data: $row);
+        }
+        $layout_dispersion[] = $row;
+
+        return $layout_dispersion;
+
+    }
+    private function datos_iniciales(Worksheet $hoja): array|stdClass
+    {
+        $es_valido = $this->es_valido(hoja: $hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $es_valido', data: $es_valido);
+        }
+        if(!$es_valido){
+            return (new errores())->error(mensaje: 'Error revise layout', data: $es_valido);
+        }
+
+        $fila_inicial = $this->fila_inicial(hoja: $hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener fila_inicial', data: $fila_inicial);
+        }
+
+        $ultima_fila = $this->ultima_fila(fila_inicial: $fila_inicial, hoja:  $hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $ultima_fila', data: $ultima_fila);
+        }
+
+        $data = new stdClass();
+        $data->fila_inicial = $fila_inicial;
+        $data->ultima_fila = $ultima_fila;
+
+        return $data;
+
+    }
+
+    private function datos_layout(Worksheet $hoja): stdClass
+    {
+        $cliente = $hoja->rangeToArray("D2:D6", null, true, false);
+
+        if(is_null($cliente[0][0])){
+            $cliente[0][0] = '';
+        }
+        if(is_null($cliente[1][0])){
+            $cliente[1][0] = '';
+        }
+        $name_cliente = strtoupper(trim($cliente[0][0]));
+        $periodo = strtoupper(trim($cliente[1][0]));
+
+        $datos = new stdClass();
+        $datos->name_cliente = $name_cliente;
+        $datos->periodo = $periodo;
+
+        return $datos;
+
+    }
+    private function es_valido(Worksheet $hoja): bool|array
+    {
+        $n_fila = 1;
+        $es_valido = false;
+
+        foreach ($hoja->getRowIterator() as $fila) {
+            $celda = $hoja->getCell('A' . $fila->getRowIndex());
+            $value = $celda->getValue();
+            if(is_null($value)){
+                $value = '';
+            }
+            $value = trim($value);
+            $value = strtoupper($value);
+            if($value === 'CLAVE EMPLEADO'){
+                $es_valido = true;
+                break;
+            }
+            $n_fila++;
+            if($n_fila >= 200){
+                return (new errores())->error(mensaje: 'Error revise layout', data: $n_fila);
+            }
+        }
+        return $es_valido;
+
+    }
+
+    private function estilos_wr(Worksheet $hoja): Worksheet
+    {
+        $hoja->getStyle('B:B')
+            ->getNumberFormat()
+            ->setFormatCode('@');
+
+        $hoja->getStyle('C:C')
+            ->getNumberFormat()
+            ->setFormatCode('0.00');
+
+        return $hoja;
+
+    }
+
+    private function fila_inicial(Worksheet $hoja): array|int
+    {
+        $fila_encabezado = $this->file_encabezado(hoja: $hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $fila_encabezado', data: $fila_encabezado);
+        }
+        return $fila_encabezado + 1;
+
+    }
+
+    private function file_encabezado(Worksheet $hoja): array|int
+    {
+        $fila_encabezado = 1;
+        $n_fila = 1;
+        foreach ($hoja->getRowIterator() as $fila) {
+            $celda = $hoja->getCell('A' . $fila->getRowIndex());
+            $value = $celda->getValue();
+            if(is_null($value)){
+                $value = '';
+            }
+            $value = trim($value);
+            $value = strtoupper($value);
+            if($value === 'CLAVE EMPLEADO'){
+                $fila_encabezado = $n_fila;
+                break;
+            }
+            $n_fila++;
+            if($n_fila >= 200){
+                return (new errores())->error(mensaje: 'Error revise layout', data: $n_fila);
+            }
+        }
+        return $fila_encabezado;
+
+    }
+
+    private function genera_row_fila(array $valores_fila): stdClass
+    {
+        $nombre = strtoupper(trim($valores_fila[0]['5']));
+        $clabe = strtoupper(trim($valores_fila[0]['9']));
+        if($clabe === ''){
+            $clabe = strtoupper(trim($valores_fila[0]['10']));
+        }
+        $monto = strtoupper(trim($valores_fila[0]['6']));
+        if(is_numeric($monto)){
+            $monto = round($monto,2);
+        }
+
+        $row = new stdClass();
+        $row->nombre = $nombre;
+        $row->clabe = $clabe;
+        $row->monto = $monto;
+        $row->concepto = 'PENSION POR RENTA VITALICIA';
+
+        return $row;
+
+    }
+
+    private function init_valores_fila(Worksheet $hoja, int $recorrido): array
+    {
+        $valores_fila = $hoja->rangeToArray("A$recorrido:L$recorrido", null, true, false);
+
+        if(is_null($valores_fila[0]['5'])){
+            $valores_fila[0]['5'] = '';
+        }
+        if(is_null($valores_fila[0]['6'])){
+            $valores_fila[0]['6'] = '';
+        }
+        if(is_null($valores_fila[0]['9'])){
+            $valores_fila[0]['9'] = '';
+        }
+        if(is_null($valores_fila[0]['10'])){
+            $valores_fila[0]['10'] = '';
+        }
+
+        return $valores_fila;
+
+    }
+
+    private function layout_dispersion(Worksheet $hoja, stdClass $ini): array
+    {
+        $layout_dispersion = array();
+        $recorrido = $ini->fila_inicial;
+
+        while ($recorrido <= $ini->ultima_fila) {
+            $layout_dispersion = $this->carga_row_layout(hoja: $hoja,layout_dispersion:  $layout_dispersion,recorrido: $recorrido);
+            if(errores::$error){
+                return (new errores())->error(mensaje: 'Error al obtener $layout_dispersion', data: $layout_dispersion);
+            }
+            $recorrido++;
+        }
+
+        return $layout_dispersion;
+
+    }
+
+    final public function lee_layout_base(stdClass$fc_layout_nom): array|stdClass
+    {
+        $archivo = $fc_layout_nom->doc_documento_ruta_absoluta;
+        $spreadsheet = IOFactory::load($archivo);
+        $hoja = $spreadsheet->getActiveSheet();
+
+        $es_valido = $this->es_valido(hoja: $hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $es_valido', data: $es_valido);
+        }
+        if(!$es_valido){
+            return (new errores())->error(mensaje: 'Error revise layout', data: $es_valido);
+        }
+
+        $ini = $this->datos_iniciales(hoja: $hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $ini', data: $ini);
+        }
+
+        $layout_dispersion = $this->layout_dispersion(hoja: $hoja,ini: $ini);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $layout_dispersion', data: $layout_dispersion);
+        }
+
+        $data = new stdClass();
+        $data->layout_dispersion = $layout_dispersion;
+        $data->hoja = $hoja;
+
+        return $data;
+
+    }
+
+    private function title_doc(Worksheet $hoja): array|string
+    {
+        $datos_ly = $this->datos_layout(hoja: $hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $datos_ly', data: $datos_ly);
+        }
+
+
+        $fecha = date('YmdHis');
+        return "$datos_ly->name_cliente.$datos_ly->periodo.$fecha.xlsx";
+
+    }
+
+    private function ultima_fila(int $fila_inicial, Worksheet $hoja): array|int
+    {
+        $ultima_fila = -1;
+        foreach ($hoja->getRowIterator() as $fila) {
+            $valor = $hoja->getCell('A' . $fila->getRowIndex())->getValue();
+            if (!empty($valor)) {
+                $ultima_fila = $fila->getRowIndex();
+            }
+        }
+        if($ultima_fila <= 0){
+            return (new errores())->error(mensaje: 'Error al obtener ultima fila', data: $ultima_fila);
+        }
+
+        if($ultima_fila <= $fila_inicial){
+            foreach ($hoja->getRowIterator() as $fila) {
+                $valor = $hoja->getCell('B' . $fila->getRowIndex())->getValue();
+                if (!empty($valor)) {
+                    $ultima_fila = $fila->getRowIndex();
+                }
+            }
+        }
+
+        return $ultima_fila;
+
+    }
+
+    private function write_data(Worksheet $hoja, array $layout_dispersion): Worksheet|array
+    {
+        $hoja = $this->write_keys($hoja);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $hoja_wr', data: $hoja);
+        }
+
+        $hoja = $this->write_rows($hoja,$layout_dispersion);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $hoja_wr', data: $hoja);
+        }
+        return $hoja;
+
+    }
+
+    final public function write_dispersion(Worksheet $hoja_base, array $layout_dispersion)
+    {
+        $xls = new Spreadsheet();
+        $hoja_wr = $xls->getActiveSheet();
+
+        $hoja_wr = $this->estilos_wr($hoja_wr);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $hoja_wr', data: $hoja_wr);
+        }
+
+        $hoja_wr = $this->write_data(hoja: $hoja_wr,layout_dispersion: $layout_dispersion);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $hoja_wr', data: $hoja_wr);
+        }
+
+        $hoja_wr = $this->autosize($hoja_wr);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $hoja_wr', data: $hoja_wr);
+        }
+
+        $title = $this->title_doc($hoja_base);
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al obtener $title', data: $title);
+        }
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment; filename="'.$title.'"');
+        header('Cache-Control: max-age=0');
+
+        try {
+            $writer = new Xlsx($xls);
+            $writer->save('php://output');
+            exit;
+        }
+        catch (Throwable $e) {
+            return (new errores())->error(mensaje: 'Error al guardar file', data: $e);
+        }
+
+
+    }
+
+    private function write_keys(Worksheet $hoja): Worksheet|array
+    {
+        $keys = array('Nombre','Clabe','Monto','Concepto');
+        $hoja->fromArray($keys);
+
+
+        try {
+            $hoja->getStyle('A1:Z1')->applyFromArray([
+                'font' => ['bold' => true],
+                'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER],
+                'borders' => ['bottom' => ['borderStyle' => Border::BORDER_THIN]],
+            ]);
+        }
+        catch (Throwable $e){
+            return (new errores())->error(mensaje: 'Error al cargar estilos', data: $e);
+        }
+
+        return $hoja;
+
+    }
+
+    private function write_row(Worksheet $hoja_wr, stdClass $row, int $row_ini): Worksheet
+    {
+        $hoja_wr->setCellValueExplicit("A$row_ini", $row->nombre, DataType::TYPE_STRING);
+        $hoja_wr->setCellValueExplicit("B$row_ini", $row->clabe, DataType::TYPE_STRING);
+        $hoja_wr->setCellValueExplicit("C$row_ini", $row->monto, DataType::TYPE_NUMERIC);
+        $hoja_wr->setCellValueExplicit("D$row_ini", $row->concepto, DataType::TYPE_STRING);
+
+        return $hoja_wr;
+
+    }
+
+    private function write_rows(Worksheet $hoja_wr, array $layout_dispersion): Worksheet|array
+    {
+        $row_ini = 2;
+        foreach ($layout_dispersion as $row){
+            $hoja_wr = $this->write_row($hoja_wr, $row, $row_ini);
+            if(errores::$error){
+                return (new errores())->error(mensaje: 'Error al obtener $hoja_wr', data: $hoja_wr);
+            }
+            $row_ini++;
+        }
+
+        return $hoja_wr;
+
+    }
+
+}
--- controllers/controlador_fc_layout_nom.php
+++ controllers/controlador_fc_layout_nom.php
@@ -6,17 +6,18 @@ use gamboamartin\facturacion\html\fc_layout_nom_html;
 use gamboamartin\facturacion\models\fc_layout_nom;
 use gamboamartin\system\links_menu;
 use gamboamartin\system\system;
-
 use gamboamartin\template_1\html;
 use PDO;
 use PhpOffice\PhpSpreadsheet\Cell\DataType;
 use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Reader\Xml\Style\NumberFormat;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PHPUnit\Runner\Baseline\Writer;
 use stdClass;
+use Throwable;
 
 class controlador_fc_layout_nom extends system{
 
@@ -56,7 +57,8 @@ class controlador_fc_layout_nom extends system{
         $input_descripcion= $this->html->input_descripcion(cols: 12,row_upd: new stdClass(),value_vacio: false);
 
         if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al generar $input_descripcion', data: $input_descripcion,header:  $header,ws:  $ws);
+            return $this->retorno_error(mensaje: 'Error al generar $input_descripcion',
+                data: $input_descripcion,header:  $header,ws:  $ws);
         }
         $this->inputs->descripcion = $input_descripcion;
 
@@ -75,6 +77,10 @@ class controlador_fc_layout_nom extends system{
 
     }
 
+
+
+
+
     public function genera_dispersion(bool $header, bool $ws = false)
     {
         $fc_layout_nom = (new fc_layout_nom($this->link))->registro($this->registro_id,retorno_obj: true);
@@ -83,150 +89,22 @@ class controlador_fc_layout_nom extends system{
                 mensaje: 'Error al obtener layout', data: $fc_layout_nom, header: $header, ws: $ws);
         }
 
-        $archivo = $fc_layout_nom->doc_documento_ruta_absoluta;
-        $spreadsheet = IOFactory::load($archivo);
-        $hoja = $spreadsheet->getActiveSheet();
-
-        $fila_encabezado = 1;
-        $n_fila = 1;
-        $es_valido = false;
-        foreach ($hoja->getRowIterator() as $fila) {
-            $celda = $hoja->getCell('A' . $fila->getRowIndex());
-            $value = $celda->getValue();
-            if(is_null($value)){
-                $value = '';
-            }
-            $value = trim($value);
-            $value = strtoupper($value);
-            if($value === 'CLAVE EMPLEADO'){
-                $fila_encabezado = $n_fila;
-                $es_valido = true;
-                break;
-            }
-            $n_fila++;
-            if($n_fila >= 200){
-                return $this->retorno_error(
-                    mensaje: 'Error revise layout', data: $n_fila, header: $header, ws: $ws);
-            }
+        $datos = (new _xls_dispersion())->lee_layout_base(fc_layout_nom: $fc_layout_nom);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al obtener $datos', data: $datos,
+                header: $header, ws: $ws);
         }
 
-        if(!$es_valido){
-            return $this->retorno_error(
-                mensaje: 'Error revise layout', data: $n_fila, header: $header, ws: $ws);
+        $xls = (new _xls_dispersion())->write_dispersion(hoja_base: $datos->hoja, layout_dispersion: $datos->layout_dispersion);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar $layout_dispersion', data: $xls,
+                header: $header, ws: $ws);
         }
-
-        $fila_inicial = $fila_encabezado + 1;
-        $ultima_fila = null;
-        foreach ($hoja->getRowIterator() as $fila) {
-            $valor = $hoja->getCell('A' . $fila->getRowIndex())->getValue();
-            if (!empty($valor)) {
-                $ultima_fila = $fila->getRowIndex();
-            }
-        }
-
-        $layout_dispersion = array();
-
-        $recorrido = $fila_inicial;
-
-        while ($recorrido <= $ultima_fila) {
-            $valores_fila = $hoja->rangeToArray("A$recorrido:L$recorrido", null, true, false);
-            $row = new stdClass();
-            if(is_null($valores_fila[0]['5'])){
-                $valores_fila[0]['5'] = '';
-            }
-            if(is_null($valores_fila[0]['9'])){
-                $valores_fila[0]['9'] = '';
-            }
-            if(is_null($valores_fila[0]['6'])){
-                $valores_fila[0]['6'] = '';
-            }
-            $nombre = strtoupper(trim($valores_fila[0]['5']));
-            $clabe = strtoupper(trim($valores_fila[0]['9']));
-            $monto = strtoupper(trim($valores_fila[0]['6']));
-            if(is_numeric($monto)){
-                $monto = round($monto,2);
-            }
-
-
-            if($clabe === ''){
-                if(is_null($valores_fila[0]['10'])){
-                    $valores_fila[0]['10'] = '';
-                }
-                $clabe = strtoupper(trim($valores_fila[0]['10']));
-            }
-
-            $row->nombre = $nombre;
-            $row->clabe = $clabe;
-            $row->monto = $monto;
-            $row->concepto = 'PENSION POR RENTA VITALICIA';
-
-            $layout_dispersion[] = $row;
-
-            $recorrido++;
-
-        }
-
-        $cliente = $hoja->rangeToArray("D2:D6", null, true, false);
-
-        if(is_null($cliente[0][0])){
-            $cliente[0][0] = '';
-        }
-        if(is_null($cliente[1][0])){
-            $cliente[1][0] = '';
-        }
-
-        $name_cliente = strtoupper(trim($cliente[0][0]));
-        $periodo = strtoupper(trim($cliente[1][0]));
-        $fecha = date('YmdHis');
-        $title = "$name_cliente.$periodo.$fecha.xlsx";
-
-        $keys = array('Nombre','Clabe','Monto','Concepto');
-
-        $xls = new Spreadsheet();
-        $hoja = $xls->getActiveSheet();
-
-        $hoja->getStyle('B:B')
-            ->getNumberFormat()
-            ->setFormatCode('@');
-
-        $hoja->getStyle('C:C')
-            ->getNumberFormat()
-            ->setFormatCode('0.00');
-
-        $hoja->fromArray($keys);
-
-        $row_ini = 2;
-        foreach ($layout_dispersion as $row){
-
-            $hoja->setCellValueExplicit("A$row_ini", $row->nombre, DataType::TYPE_STRING);
-            $hoja->setCellValueExplicit("B$row_ini", $row->clabe, DataType::TYPE_STRING);
-            $hoja->setCellValueExplicit("C$row_ini", $row->monto, DataType::TYPE_NUMERIC);
-            $hoja->setCellValueExplicit("D$row_ini", $row->concepto, DataType::TYPE_STRING);
-
-            $row_ini++;
-        }
-
-        $hoja->getStyle('A1:Z1')->applyFromArray([
-            'font' => ['bold' => true],
-            'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER],
-            'borders' => ['bottom' => ['borderStyle' => Border::BORDER_THIN]],
-        ]);
-
-        $hoja->getColumnDimension('A')->setAutoSize(true);
-        $hoja->getColumnDimension('B')->setAutoSize(true);
-        $hoja->getColumnDimension('C')->setAutoSize(true);
-        $hoja->getColumnDimension('D')->setAutoSize(true);
-
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment; filename="'.$title.'"');
-        header('Cache-Control: max-age=0');
-
-        $writer = new Xlsx($xls);
-        $writer->save('php://output');
         exit;
 
 
     }
+
 
 
 }
