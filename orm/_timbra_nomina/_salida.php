e28f881f40e45117479a79ae4f6398b722e750f3
corporativoem3 <desarrollo@corporativoem3.com.mx>
2025-09-22 11:13:37 -0600

    x

--- orm/_timbra_nomina/_salida.php
+++ orm/_timbra_nomina/_salida.php
@@ -20,7 +20,7 @@ class _salida{
     final public function code_error(string $codigo, stdClass $rs, string $nomina_json, PDO $link, int $fc_row_layout_id)
     {
         if($codigo !== '200'){
-
+            $con_error = true;
             $JSON = json_decode($nomina_json,false);
             $extra_data = '';
             if($codigo === 'CFDI40145'){
@@ -28,11 +28,18 @@ class _salida{
                 $extra_data .=" Nombre: {$JSON->Comprobante->Receptor->Nombre}";
             }
 
+            if($codigo === 'CFDI40999'){
+                $extra_data ="RFC: {$JSON->Comprobante->Receptor->Rfc}";
+                $extra_data .=" Nombre: {$JSON->Comprobante->Receptor->Nombre}";
+                $extra_data .=" DomicilioFiscalReceptor: CON ERROR";
+            }
+
             if($codigo === '307'){
+                $con_error = false;
                 errores::$error = false;
             }
 
-            else {
+            if($con_error){
                 $upd = $this->upd_error($codigo, $rs, $link,$fc_row_layout_id);
                 return (new errores())->error("Error al timbrar $rs->mensaje Code: $rs->codigo $extra_data", $upd);
             }
