514d09a825494b9f692001a8a396872a2da9fb24
Ricardo Rivera <ricardo.rivera@ivitec.com.mx>
2025-11-06 16:16:22 -0600

    nuevos controladores com_contacto y com_tipo_contacto

--- controllers/controlador_com_cliente.php
+++ controllers/controlador_com_cliente.php
@@ -8,18 +8,29 @@
  */
 namespace gamboamartin\facturacion\controllers;
 
+use gamboamartin\errores\errores;
 use gamboamartin\facturacion\models\com_cliente;
 use gamboamartin\template\html;
 use PDO;
 use stdClass;
 
 class controlador_com_cliente extends \gamboamartin\comercial\controllers\controlador_com_cliente {
+    public string $link_asigna_contacto_bd = '';
     public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(),
                                 stdClass $paths_conf = new stdClass())
     {
         parent::__construct(link: $link,html:  $html,paths_conf:  $paths_conf);
 
         $this->modelo = new com_cliente(link: $this->link);
+
+        $link_asigna_contacto_bd = $this->obj_link->link_alta_bd(link: $this->link, seccion: 'com_contacto');
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al obtener link', data: $link_asigna_contacto_bd);
+            print_r($error);
+            exit;
+        }
+
+        $this->link_asigna_contacto_bd = $link_asigna_contacto_bd;
 
 
     }
---
+++ controllers/controlador_com_contacto.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\facturacion\controllers;
+
+use gamboamartin\comercial\models\com_tipo_contacto;
+use gamboamartin\template\html;
+use PDO;
+use stdClass;
+
+class controlador_com_contacto extends \gamboamartin\comercial\controllers\controlador_com_contacto {}
