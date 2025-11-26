d33b2265be177588323fb20962df4a6ce0645ccf
Ricardo Rivera <ricardo.rivera@ivitec.com.mx>
2025-11-25 18:19:47 -0600

    FTR validacion de correo del contacto del cliente

--- controllers/controlador_com_cliente.php
+++ controllers/controlador_com_cliente.php
@@ -34,4 +34,53 @@ class controlador_com_cliente extends \gamboamartin\comercial\controllers\contro
 
 
     }
+
+    public function asigna_contacto(bool $header, bool $ws = false, array $not_actions = array()): array|string
+    {
+        $this->accion_titulo = 'Asignar contacto';
+
+        $r_modifica = $this->init_modifica();
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al generar salida de template', data: $r_modifica, header: $header, ws: $ws);
+        }
+
+        $keys_selects = $this->init_selects_inputs();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar selects', data: $keys_selects, header: $header,
+                ws: $ws);
+        }
+
+        $this->row_upd->telefono = '';
+
+        $base = $this->base_upd(keys_selects: $keys_selects, params: array(), params_ajustados: array());
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al integrar base', data: $base, header: $header, ws: $ws);
+        }
+
+        $button = $this->html->button_href(accion: 'modifica', etiqueta: 'Ir a Cliente',
+            registro_id: $this->registro_id, seccion: $this->tabla, style: 'warning', params: array());
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al generar link', data: $button);
+        }
+
+        $this->button_com_cliente_modifica = $button;
+
+        $data_view = new stdClass();
+        $data_view->names = array('Id', 'Tipo', 'Contacto', 'Teléfono', 'Correo','Validacion Correo', 'Acciones');
+        $data_view->keys_data = array('com_contacto_id', 'com_tipo_contacto_descripcion', 'com_contacto_descripcion',
+            'com_contacto_telefono', 'com_contacto_correo', 'com_contacto_estatus_correo');
+        $data_view->key_actions = 'acciones';
+        $data_view->namespace_model = 'gamboamartin\\comercial\\models';
+        $data_view->name_model_children = 'com_contacto';
+
+        $contenido_table = $this->contenido_children(data_view: $data_view, next_accion: __FUNCTION__,
+            not_actions: $not_actions);
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al obtener tbody', data: $contenido_table, header: $header, ws: $ws);
+        }
+
+        return $contenido_table;
+    }
 }
