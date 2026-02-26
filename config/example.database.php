e9c46d23fa621efc1491e9f7e6efb066e9f6684a
Ricardo Rivera <ricardo.rivera@ivitec.com.mx>
2026-02-26 13:46:30 -0600

    nuevo archivo example de config

---
+++ config/example.database.php
@@ -0,0 +1,11 @@
+<?php
+namespace config;
+class database{
+    public string $db_name ='facturacion';
+    public string $db_user = 'root';
+    public string $db_password = '12345678';
+    public string $db_host = 'localhost';
+    public string $set_name = 'utf8';
+    public string $sql_mode = '';
+    public int $time_out = 100;
+}
