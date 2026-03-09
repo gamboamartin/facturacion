b3ff4f0e45fee7b4fcebc8528acb94713e8ed088
Ricardo Rivera <ricardo.rivera@ivitec.com.mx>
2026-03-09 17:38:47 -0600

    working factura de resguardo

--- orm/fc_layout_factura.php
+++ orm/fc_layout_factura.php
@@ -4,6 +4,7 @@ use base\orm\modelo;
 use gamboamartin\errores\errores;
 use gamboamartin\proceso\models\pr_entidad;
 use PDO;
+use PDOException;
 
 
 class fc_layout_factura extends modelo{
@@ -29,17 +30,21 @@ class fc_layout_factura extends modelo{
     {
         $this->link->beginTransaction();
 
-        $rs1 = $this->elimina_relacion_con_factura_id(fc_factura_id: $fc_factura_id);
+        $r_montos = $this->actualiza_montos_por_asignar(
+            fc_factura_id:  $fc_factura_id,
+            fc_layout_nom_id: $fc_layout_nom_id
+        );
         if(errores::$error){
             $this->link->rollBack();
             return $this->error->error(
-                mensaje: 'Error en elimina_relacion_con_factura_id',data: $rs1
+                mensaje: 'Error al modificar montos por asignar',data: $r_montos
             );
         }
 
         $rs2 = $this->relaciona_factura_y_layout(
             fc_factura_id:  $fc_factura_id,
-            fc_layout_nom_id: $fc_layout_nom_id
+            fc_layout_nom_id: $fc_layout_nom_id,
+            montos: $r_montos
         );
         if(errores::$error){
             $this->link->rollBack();
@@ -57,17 +62,21 @@ class fc_layout_factura extends modelo{
     {
         $this->link->beginTransaction();
 
-        $rs1 = $this->elimina_relacion_con_layout_nom_id(fc_layout_nom_id: $fc_layout_nom_id);
+        $r_montos = $this->actualiza_montos_por_asignar(
+            fc_factura_id:  $fc_factura_id,
+            fc_layout_nom_id: $fc_layout_nom_id
+        );
         if(errores::$error){
             $this->link->rollBack();
             return $this->error->error(
-                mensaje: 'Error en elimina_relacion_con_layout_nom_id',data: $rs1
+                mensaje: 'Error al modificar montos por asignar',data: $r_montos
             );
         }
 
         $rs2 = $this->relaciona_factura_y_layout(
             fc_factura_id:  $fc_factura_id,
-            fc_layout_nom_id: $fc_layout_nom_id
+            fc_layout_nom_id: $fc_layout_nom_id,
+            montos: $r_montos
         );
         if(errores::$error){
             $this->link->rollBack();
@@ -76,18 +85,33 @@ class fc_layout_factura extends modelo{
             );
         }
 
+
+
         $this->link->commit();
 
         return [];
     }
 
-    private function relaciona_factura_y_layout(int $fc_factura_id, int $fc_layout_nom_id){
+    private function relaciona_factura_y_layout(int $fc_factura_id, int $fc_layout_nom_id, array $montos){
 
         $user_id = $_SESSION['usuario_id'];
 
+        $resto_factura = 0;
+        $resto_layout_nom = 0;
+
+        if (isset($montos['monto_resto_factura'])) {
+            $resto_factura = $montos['monto_resto_factura'];
+        }
+
+        if (isset($montos['monto_resto_layout'])) {
+            $resto_layout_nom = $montos['monto_resto_layout'];
+        }
+
         $consulta = "INSERT INTO fc_layout_factura ";
-        $consulta .= " (fc_layout_nom_id, fc_factura_id, status, usuario_alta_id, usuario_update_id)";
-        $consulta .= " VALUES ({$fc_layout_nom_id}, {$fc_factura_id}, 'activo', {$user_id}, {$user_id})";
+        $consulta .= " (fc_layout_nom_id, fc_factura_id, status, usuario_alta_id, usuario_update_id, ";
+        $consulta .= " resto_asignar_factura, resto_asignar_layout_nom)";
+        $consulta .= " VALUES ({$fc_layout_nom_id}, {$fc_factura_id}, 'activo', {$user_id}, {$user_id}, ";
+        $consulta .= " {$resto_factura}, {$resto_layout_nom})";
         $rs = $this->ejecuta_sql($consulta);
         if(errores::$error){
             return $this->error->error(
@@ -114,6 +138,120 @@ class fc_layout_factura extends modelo{
 
         return [];
 
+    }
+
+    private function actualiza_montos_por_asignar(int $fc_factura_id, int $fc_layout_nom_id)
+    {
+        $fc_factura_modelo = new fc_factura($this->link);
+        $fc_layout_nom_modelo = new fc_layout_nom($this->link);
+
+        $fc_factura_modelo->registro_id = $fc_factura_id;
+        $data_factura = $fc_factura_modelo->obten_data();
+        if(errores::$error){
+            return $this->error->error(
+                mensaje: 'Error al obtener el monto_por_asignar de la factura', data: $data_factura
+            );
+        }
+
+        $fc_layout_nom_modelo->registro_id = $fc_layout_nom_id;
+        $data_layout = $fc_layout_nom_modelo->obten_data();
+        if(errores::$error){
+            return $this->error->error(
+                mensaje: 'Error al obtener el monto_por_asignar de la layout', data: $data_layout
+            );
+        }
+
+        $monto_factura = (double)$data_factura['fc_factura_monto_por_asignar'];
+        $monto_layout = (double)$data_layout['fc_layout_nom_monto_por_asignar'];
+
+        $resto_layout = 0;
+        $resto_factura = 0;
+
+        if ($monto_factura > $monto_layout) {
+
+            $resto_layout = 0;
+            $resto_factura = $monto_factura - $monto_layout;
+
+            $rs_mf = $this->modifica_monto_por_asignar(
+                tabla: 'fc_factura',
+                nuevo_monto: $resto_factura,
+                registro_id: $fc_factura_id
+            );
+            if(errores::$error){
+                return $this->error->error(
+                    mensaje: 'Error al modificar el monto por asignar de la factura', data: $rs_mf
+                );
+            }
+
+            $rs_ml = $this->modifica_monto_por_asignar(
+                tabla: 'fc_layout_nom',
+                nuevo_monto: $resto_layout,
+                registro_id: $fc_layout_nom_id
+            );
+            if(errores::$error){
+                return $this->error->error(
+                    mensaje: 'Error al modificar el monto por asignar de el layout', data: $rs_ml
+                );
+            }
+        }
+
+        if ($monto_layout > $monto_factura) {
+
+            $resto_layout = $monto_layout - $monto_factura;
+            $resto_factura = 0;
+
+            $rs_mf = $this->modifica_monto_por_asignar(
+                tabla: 'fc_factura',
+                nuevo_monto: $resto_factura,
+                registro_id: $fc_factura_id
+            );
+            if(errores::$error){
+                return $this->error->error(
+                    mensaje: 'Error al modificar el monto por asignar de la factura', data: $rs_mf
+                );
+            }
+
+            $rs_ml = $this->modifica_monto_por_asignar(
+                tabla: 'fc_layout_nom',
+                nuevo_monto: $resto_layout,
+                registro_id: $fc_layout_nom_id
+            );
+            if(errores::$error){
+                return $this->error->error(
+                    mensaje: 'Error al modificar el monto por asignar de el layout', data: $rs_ml
+                );
+            }
+        }
+
+        if ($monto_factura === $monto_layout){
+
+            $rs_mf = $this->modifica_monto_por_asignar(
+                tabla: 'fc_factura',
+                nuevo_monto: 0,
+                registro_id: $fc_factura_id
+            );
+            if(errores::$error){
+                return $this->error->error(
+                    mensaje: 'Error al modificar el monto por asignar de la factura', data: $rs_mf
+                );
+            }
+
+            $rs_ml = $this->modifica_monto_por_asignar(
+                tabla: 'fc_layout_nom',
+                nuevo_monto: 0,
+                registro_id: $fc_layout_nom_id
+            );
+            if(errores::$error){
+                return $this->error->error(
+                    mensaje: 'Error al modificar el monto por asignar de el layout', data: $rs_ml
+                );
+            }
+        }
+
+        return [
+            'monto_resto_factura' => $resto_factura,
+            'monto_resto_layout' => $resto_layout,
+        ];
     }
 
     public function elimina_relacion_con_factura_id(int $fc_factura_id)
@@ -195,6 +333,32 @@ class fc_layout_factura extends modelo{
                 );
             }
 
+        }
+
+        return [];
+    }
+
+    private function modifica_monto_por_asignar(string $tabla, $nuevo_monto, int $registro_id): array
+    {
+        $query = "
+                UPDATE 
+                    {$tabla}
+                SET 
+                    {$tabla}.monto_por_asignar = :nuevo_monto
+                WHERE 
+                    {$tabla}.id = :registro_id";
+
+        try {
+            $stmt = $this->link->prepare($query);
+            $stmt->execute([
+                ':nuevo_monto' => $nuevo_monto,
+                ':registro_id' => $registro_id,
+            ]);
+        } catch (PDOException $e) {
+            return (new errores())->error(
+                mensaje: $e->getMessage(),
+                data:  $e
+            );
         }
 
         return [];
